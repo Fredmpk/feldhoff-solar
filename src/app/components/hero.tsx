11d2fd371ebd1708b2258b97b766164cdb09d4ae
Fred <frederik.koenig@posteo.de>
2025-06-23 15:42:29 +0200

    fix: finished navbar

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -4,11 +4,11 @@ export function Hero() {
       className="pt-16 sm:pt-20 bg-[url('/img/hero-bg.png')] 
     bg-cover bg-top md:bg-center bg-no-repeat md:h-screen h-[60vh] flex flex-col  items-center"
     >
-      <h2 className="flex text-center justify-center font-bold m-6 sm:m-8 md:m-16 lg:m-18 sm:text-xl md:text-3xl lg:text-3xl xl:text-4xl">
+      <h1 className="flex text-center justify-center font-bold m-6 sm:m-8 md:m-16 lg:m-18 sm:text-xl md:text-3xl lg:text-3xl xl:text-4xl tracking-wide">
         Komplettlösungen für Photovoltaik und Energiesystemen – von der Planung
         bis zur Inbetriebnahme.
-      </h2>
-      <button className="flex text-center font-bold text-bottom sm:text-xl md:text-3xl lg:text-3xl xl:text-4xl bg-tforange p-3 rounded-full text-white">
+      </h1>
+      <button className="flex text-center font-bold text-bottom sm:text-xl md:text-3xl lg:text-3xl xl:text-4xl bg-tforange p-3 rounded-full text-white tracking-wide">
         Jetzt kontaktieren
       </button>
     </section>
--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,25 +4,28 @@ import { BurgerMenu } from "./burger-menu";
 export function Navbar() {
   return (
     <div className="fixed w-full bg-white opacity-90 shadow-md z-50 ">
-      <nav className="flex items-center justify-between align-items text-lg md:text-2xl tracking-wide h-16 sm:h-20 px-3 max-w-screen overflow-hidden">
+      <nav className="flex items-center justify-between align-items text-lg md:text-2xl tracking-wide h-20 sm:h-32 px-3 max-w-screen overflow-hidden">
         <Link
           href="/"
-          className="absolute left-1/2 transform -translate-x-1/2 md:relative md:left-auto md:transform-none md:-translate-x-0 lg:absolute lg:left-1/2 lg:transform lg:-translate-x-1/2"
+          className="absolute left-1/2 transform -translate-x-1/2 md:relative md:left-auto md:transform-none md:-translate-x-0 "
         >
           <img
-            className="w-auto h-16 object-contain sm:p-3"
-            src="/img/logo-cut.png"
+            className="w-auto h-20 sm:h-28 object-contain sm:pl-3 pt-1 pr-8 pb-3 md:pl-8 lg:pl-16"
+            src="/img/logo-sonne.png"
           ></img>
         </Link>
 
-        <ul className="items-center gap-4 lg:gap-9 hidden md:flex ml-auto">
+        <ul className=" gap-5 md:gap-10 lg:gap-20 pt-9 lg:pt-8 hidden md:flex ml-auto font-bold md:text-sm lg:text-md xl:text-lg pr-3 tracking-widest">
           <li>
-            <Link href="/#gallery">Galerie</Link>
+            <Link href="/#offers">Leistungen</Link>
           </li>
           <li>
-            <Link href="/#about">Über uns</Link>
+            <Link href="/#advantages">Vorteile</Link>
           </li>
 
+          <li>
+            <Link href="/#company">Unternehmen</Link>
+          </li>
           <li>
             <Link href="/#contact">Kontakt</Link>
           </li>
