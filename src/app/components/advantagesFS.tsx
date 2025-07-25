a077281a48b58228efd6bfff8ce368f5500f3208
Fred <frederik.koenig@posteo.de>
2025-07-25 15:40:36 +0200

    fix: finished the desktop version of advantages

--- src/app/components/advantagesFS.tsx
+++ src/app/components/advantagesFS.tsx
@@ -6,6 +6,11 @@ export default function AdvantagesFS({
 }: {
   advantagesFS: ADVANTAGES_FS_QUERYResult;
 }) {
+  // const text = advantagesFS?.titleS3?.replace(
+  //   /Ganzheitliche/gi,
+  //   "Ganz" + String.fromCharCode(0x00ad) + "heitliche"
+  // );
+
   return (
     <div className="flex flex-col items-center justify-center">
       <h2 className=" font-bold text-center text-2xl md:text-3xl lg:text-3xl xl:text-4xl tracking-wide py-8 md:py-12 lg:py-16">
@@ -17,9 +22,9 @@ export default function AdvantagesFS({
         </h3>
         <div className="flex flex-col gap-4 md:gap-8 lg:gap-12 xl:gap-16">
           <div className="flex flex-row gap-4 md:gap-8 lg:gap-12 xl:gap-16">
-            <div className="bg-tforange rounded-xl w-52 h-52 md:w-68 md:h-64 lg:w-72 lg:h-72 xl:w-82 xl:h-82  p-2 md:p-4 lg:p-6 xl:p-8 relative">
-              <div className="pr-2 md:pr-4 lg:pr-6 ">
-                <h4 className="text-white text-xl font-bold md:text-2xl lg:text-3xl pr-4">
+            <div className="bg-tforange rounded-xl w-52 h-52 md:w-68 md:h-64 lg:w-72 lg:h-72 xl:w-82 xl:h-82  p-2 md:p-3 lg:p-4 xl:p-5 relative">
+              <div className="pl-2 md:pl-3 lg:pl-5 ">
+                <h4 className="text-white text-xl font-bold md:text-2xl lg:text-3xl">
                   {advantagesFS?.titleS1}
                 </h4>
                 <p className="text-white text-md font-semibold md:text-lg lg:text-xl pt-1 md:pt-2 ">
@@ -32,8 +37,8 @@ export default function AdvantagesFS({
               </div>
             </div>
             <div className="bg-tfturquoise rounded-xl p-2 md:p-3 lg:p-4 xl:p-5 flex  justify-center w-52 h-52 md:w-68 md:h-64 lg:w-72 lg:h-72 xl:w-82 xl:h-82 relative">
-              <div className="pr-2 md:pl-3 lg:pl-5 ">
-                <h4 className="text-white text-right text-xl font-bold md:text-2xl lg:text-3xl pl-8">
+              <div className="pr-2 md:pr-3 lg:pr-5">
+                <h4 className="text-white text-right text-xl font-bold md:text-2xl lg:text-3xl">
                   {advantagesFS?.titleS2}
                 </h4>
                 <p className="text-white text-right text-md font-semibold md:text-lg lg:text-xl pt-1 md:pt-2 ">
@@ -47,32 +52,32 @@ export default function AdvantagesFS({
             </div>
           </div>
           <div className="flex flex-row gap-4 md:gap-8 lg:gap-12 xl:gap-16">
-            <div className="bg-tforange rounded-xl p-2 md:p-4 lg:p-6 xl:p-8 flex items-center justify-center w-52 h-52 md:w-68 md:h-64 lg:w-72 lg:h-72 xl:w-82 xl:h-82">
-              <img
-                src={urlFor(advantagesFS?.iconS3).url()}
-                className="w-16 h-16 md:w-24 md:h-24 lg:w-32 lg:h-32 xl:w-40 xl:h-40"
-              />
-              <div className="pl-2 md:pl-4 lg:pl-6 xl:pl-8">
-                <h4 className="text-white text-xs md:text-sm lg:text-md xl:text-lg">
+            <div className="bg-tforange rounded-xl p-2 md:p-3 lg:p-4 xl:p-5 flex  justify-center items-end w-52 h-52 md:w-68 md:h-64 lg:w-72 lg:h-72 xl:w-82 xl:h-82 relative">
+              <div className="pl-2 md:pl-3 lg:pl-5">
+                <h4 className="text-white text-left text-xl font-bold md:text-2xl lg:text-3xl lg:w-42 ">
                   {advantagesFS?.titleS3}
                 </h4>
-                <p className="text-white text-xs md:text-sm lg:text-md xl:text-lg">
+                <img
+                  src={urlFor(advantagesFS?.iconS3).url()}
+                  className="w-8 h-8 md:w-12 md:h-12 lg:w-14 lg:h-14 xl:w-20 xl:h-20 flex-shrink-0 absolute top-4 right-4 md:top-4 md:right-4 xl:top-8 xl:right-8 "
+                />
+                <p className="text-white text-left text-md font-semibold md:text-lg lg:text-xl pt-1 md:pt-2  ">
                   {advantagesFS?.textS3}
                 </p>
               </div>
             </div>
-            <div className="bg-tfturquoise rounded-xl p-2 md:p-4 lg:p-6 xl:p-8 flex items-center justify-center w-52 h-52 md:w-68 md:h-64 lg:w-64 lg:h-64 xl:w-82 xl:h-82">
-              <div className="pr-2 md:pr-4 lg:pr-6 xl:pr-8">
-                <h4 className="text-white text-xs md:text-sm lg:text-md xl:text-lg">
+            <div className="bg-tfturquoise rounded-xl p-2 md:p-3 lg:p-4 xl:p-5 flex justify-center items-end w-52 h-52 md:w-68 md:h-64 lg:w-72 lg:h-72 xl:w-82 xl:h-82 relative">
+              <div className="pr-2 md:pr-3 lg:pr-5">
+                <h4 className="text-white text-right text-xl font-bold md:text-2xl lg:text-3xl">
                   {advantagesFS?.titleS4}
                 </h4>
-                <p className="text-white text-xs md:text-sm lg:text-md xl:text-lg">
+                <p className="text-white text-right text-md font-semibold md:text-lg lg:text-xl pt-1 md:pt-2 ">
                   {advantagesFS?.textS4}
                 </p>
               </div>
               <img
                 src={urlFor(advantagesFS?.iconS4).url()}
-                className="w-16 h-16 md:w-24 md:h-24 lg:w-32 lg:h-32 xl:w-40 xl:h-40"
+                className="w-8 h-8 md:w-12 md:h-12 lg:w-14 lg:h-14 xl:w-20 xl:h-20 flex-shrink-0 absolute top-4 left-4 md:top-4 md:left-4 xl:top-8 xl:left-8 "
               />
             </div>
           </div>
