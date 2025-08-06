541188df0b663f657b83ce4f131baac858403f05
Fred <frederik.koenig@posteo.de>
2025-08-06 12:43:50 +0200

    added sanity live token

--- src/sanity/live.ts
+++ src/sanity/live.ts
@@ -1,9 +1,10 @@
 import { defineLive } from "next-sanity";
 import { client } from "@/sanity/client";
+import { token } from "./env";
 // import { token } from "./env";
 
 export const { sanityFetch, SanityLive } = defineLive({
   client: client.withConfig({ apiVersion: "2025-06-24" }),
-  //   serverToken: token,
-  //   browserToken: token,
+  serverToken: token,
+  browserToken: token,
 });
